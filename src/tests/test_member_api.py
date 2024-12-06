53999074dcfb564944f11f8dd293c07a2d973c6c
mountain-kangkang <rngl10@naver.com>
2024-12-06 15:33:24 +0900

    뭐가 바뀐거임?

--- src/tests/test_member_api.py
+++ src/tests/test_member_api.py
@@ -14,7 +14,6 @@ def test_health_check(client):
 def test_user_sign_up(client, test_session):
     # given
 
-
     # when
     response = client.post(
         "/sync/members",
@@ -33,7 +32,6 @@ def test_user_sign_up(client, test_session):
 
 def test_user_login(client, test_session, test_user):
     # given
-    new_user = test_user
 
     # when
     credentials = b"test_user:pw"
@@ -49,7 +47,7 @@ def test_user_login(client, test_session, test_user):
     assert response.json()["access_token"]
 
 
-def test_get_me(client, test_session, test_access_token):
+def test_get_me(client, test_session, test_user, test_access_token):
     # given
 
     # when
@@ -60,13 +58,14 @@ def test_get_me(client, test_session, test_access_token):
 
     # then
     assert response.status_code == 200
-    assert response.json()["id"] == new_user.id
-    assert response.json()["username"] == new_user.username
-    assert response.json()["password"] == new_user.password
+    assert response.json()["id"] == test_user.id
+    assert response.json()["username"] == test_user.username
+    assert response.json()["password"] == test_user.password
 
 
-def test_update_me(client, test_session, test_access_token):
+def test_update_me(client, test_session, test_user, test_access_token):
     # given
+    old_password = test_user.password
 
     # when
     response = client.patch(
@@ -77,8 +76,8 @@ def test_update_me(client, test_session, test_access_token):
 
     # then
     assert response.status_code == 200
-    assert response.json()["id"] == new_user.id
-    assert response.json()["username"] == new_user.username
+    assert response.json()["id"] == test_user.id
+    assert response.json()["username"] == test_user.username
     assert response.json()["password"] != old_password
 
 
